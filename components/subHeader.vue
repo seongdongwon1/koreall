5952f06d816c29904fbb80c61ce6ebee89b088e8
SD <sdtv9999@gmail.com>
2022-09-01 16:03:31 +0900

    날씨에 대한 api 이용하기 성공적...?

--- components/subHeader.vue
+++ components/subHeader.vue
@@ -7,90 +7,123 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'SubHeader',
     data () {
         return {
-            weather: {
-                1: '서울특별시',
-                2: '부산광역시',
-                3: '대구광역시',
-                4: '인천광역시',
-                5: '광주광역시',
-                6: '대전광역시',
-                7: '울산광역시',
-                8: '경기도',
-                9: '강원도',
-                10: '충청북도',
-                11: '충청남도',
-                12: '경상북도',
-                13: '경상남도',
-                14: '전라북도',
-                15: '전라남도',
-                16: '제주특별자치도',
-                17: '세종특별자치도'
-            },
+            weather: [
+                '서울특별시',
+                '부산광역시',
+                '대구광역시',
+                '인천광역시',
+                '광주광역시',
+                '대전광역시',
+                '울산광역시',
+                '경기도',
+                '강원도',
+                '충청북도',
+                '충청남도',
+                '경상북도',
+                '경상남도',
+                '전라북도',
+                '전라남도',
+                '제주특별자치도',
+                '세종특별자치도'
+            ],
             matchingCode: {
                 서울특별시: {
-                    nx: '60', ny: '127'
+                    nx: 60, ny: 127
                 },
                 부산광역시: {
-                    nx: '98', ny: '76'
+                    nx: 98, ny: 76
                 },
                 대구광역시: {
-                    nx: '89', ny: '90'
+                    nx: 89, ny: 90
                 },
                 인천광역시: {
-                    nx: '55', ny: '124'
+                    nx: 55, ny: 124
                 },
                 광주광역시: {
-                    nx: '58', ny: '74'
+                    nx: 58, ny: 74
                 },
                 대전광역시: {
-                    nx: '67', ny: '100'
+                    nx: 67, ny: 100
                 },
                 울산광역시: {
-                    nx: '102', ny: '84'
+                    nx: 102, ny: 84
                 },
                 경기도: {
-                    nx: '60', ny: '120'
+                    nx: 60, ny: 120
                 },
                 강원도: {
-                    nx: '73', ny: '134'
+                    nx: 73, ny: 134
                 },
                 충청북도: {
-                    nx: '69', ny: '107'
+                    nx: 69, ny: 107
                 },
                 충청남도: {
-                    nx: '68', ny: '100'
+                    nx: 68, ny: 100
                 },
                 경상북도: {
-                    nx: '89', ny: '91'
+                    nx: 89, ny: 91
                 },
                 경상남도: {
-                    nx: '91', ny: '77'
+                    nx: 91, ny: 77
                 },
                 전라북도: {
-                    nx: '63', ny: '89'
+                    nx: 63, ny: 89
                 },
                 전라남도: {
-                    nx: '51', ny: '67'
+                    nx: 51, ny: 67
                 },
                 제주특별자치도: {
-                    nx: '52', ny: '38'
+                    nx: 52, ny: 38
                 },
                 세종특별자치도: {
-                    nx: '66', ny: '103'
+                    nx: 66, ny: 103
                 }
-            }
+            },
+            apiKey: 'pYO1nVorD2xTWG%2F6GzmC3bRYZoOOMyJ0YbEG3ByX4l22EpmZoYkFO6UsMt%2BDM5PKg9BpyLbSiVN5HMIaf6PUTQ%3D%3D',
+            inputData: [],
+            tDate: '',
+            tTime: ''
         }
     },
     mounted () {
-        this.getWeatherData()
+        this.getTodayInfo()
     },
     methods: {
-        getWeatherData () {
-            console.log('111')
+        getTodayInfo () {
+            const nowDate = new Date()
+            const yyyy = nowDate.getFullYear().toString()
+            const MM = this.pad(nowDate.getMonth() + 1, 2)
+            const dd = this.pad(nowDate.getDate(), 2)
+            const hh = this.pad(nowDate.getHours(), 2)
+            const mm = '00'
+
+            this.tDate = yyyy + MM + dd
+            this.tTime = hh + mm
+            this.getWeatherData()
+        },
+        pad (number, length) {
+            let str = '' + number
+            while (str.length < length) {
+                str = '0' + str
+            }
+            return str
+        },
+        async getWeatherData () {
+            this.inputData = []
+            for (let i = 0; i < this.weather.length; i++) {
+                await axios.get('http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=' + this.apiKey + '&numOfRows=10&pageNo=1&base_date=' + this.tDate + '&base_time=' + this.tTime + '&nx=' + this.matchingCode[this.weather[i]].nx + '&ny=' + this.matchingCode[this.weather[i]].ny + '&category=T1H&dataType=json')
+                    .then((res) => {
+                        console.log('res' + this.weather[i], res.data)
+                    }).catch((err) => {
+                        console.log('err', err)
+                    })
+            }
         }
     }
 }
