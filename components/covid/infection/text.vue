5a13d55b578893bcb9f429f3bee6ed4a669fe721
SD <sdtv9999@gmail.com>
2022-09-15 12:29:29 +0900

    [new] 감염현황 표 생성

---
+++ components/covid/infection/text.vue
@@ -0,0 +1,47 @@
+<template>
+  <table style="width: 100%;">
+    <thead>
+      <tr>
+        <th>오늘 확진자</th>
+        <th>오늘 사망자</th>
+        <th>누적 확진자</th>
+        <th>누적 사망자</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>{{ infection.today.infection | formatNum }}명</td>
+        <td>{{ infection.today.death | formatNum }}명</td>
+        <td>{{ infection.total.infection | formatNum }}명</td>
+        <td>{{ infection.total.death | formatNum }}명</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+    name: 'InfectionText',
+    props: ['infection']
+}
+</script>
+
+<style scoped>
+    table thead tr th {
+        padding: 6.5px 5px;
+        border: 1px solid #504e4e;
+        border-bottom-color: #504e4e;
+        font-weight: bold;
+        background-color: #95abdd;
+        text-align: center;
+        color: white;
+        font-size: 14px;
+    }
+    table tbody tr td {
+        padding: 6.5px 5px;
+        border: 1px solid #504e4e;
+        font-weight: normal;
+        text-align: center;
+        font-size: 14px;
+    }
+</style>
