fecbb44c5b2a3c87ca4c696f8e3afc29e1520003
SD <sdtv9999@gmail.com>
2022-09-19 12:36:41 +0900

    [new] github pages 배포하기

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
     // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+    target: 'static',
+    router: {
+        base: '/koreall/'
+    },
     ssr: false,
 
     // Global page headers: https://go.nuxtjs.dev/config-head
