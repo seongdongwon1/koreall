9c48048f019b831def242bcc43aa61ea4c279ca8
SD <sdtv9999@gmail.com>
2022-09-19 13:24:53 +0900

    [improvement] 흠 이게 배포하는 과정에서 딜레이가 있는건가..

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        baseURL: '/seongdongwon1.github.io',
+        baseURL: '/',
         proxy: true
     },
     proxy: {
