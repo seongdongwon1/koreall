7409104ec0a681b07397ca94b5f8f2dfbadc8a03
SD <sdtv9999@gmail.com>
2022-09-23 10:42:12 +0900

    [test]

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        // baseURL: 'https://seongdongwon1.github.io/koreall/',
+        baseURL: 'https://seongdongwon1.github.io',
         proxy: true
         // credentials: true
     },
